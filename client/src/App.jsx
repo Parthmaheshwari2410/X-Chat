26f20f537df9e4afde6c1b1efcfb9f61404ad88e
Parth Maheshwari <parthmaheshwari2410@gmail.com>
2026-09-25 14:20:40 +0530

    fixing bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,10 @@ import React, { useContext } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ProfilePage from './pages/ProfilePage'
-import LoginPage from './pages/loginPage'
 import { Toaster } from "react-hot-toast"
 import { AuthContext } from './context/AuthContext'
+import LoginPage from './pages/LoginPage'
+
 
 
 
